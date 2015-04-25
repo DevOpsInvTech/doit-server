719898e3fbf218349b124a3105442e6ad84ef85c
Rob Cameron <rwcameron@gmail.com>
2015-04-25 10:50:57 -0700

    Update doit_server.go

--- doit_server.go
+++ doit_server.go
@@ -37,33 +37,33 @@ func (ds *DoitServer) Listen(port *string, config *DoitConfig) (err error) {
 	//home
 	r.HandleFunc("/", ds.homeHandler)
 	//templates
-	r.HandleFunc("/api/1/template/{name}", nil)
-	r.HandleFunc("/api/1/templates", nil)
+	r.HandleFunc("/api/v1/template/{name}", nil)
+	r.HandleFunc("/api/v1/templates", nil)
 	//object
-	r.HandleFunc("/api/1/object/{name}", nil)
-	r.HandleFunc("/api/1/objects", nil)
+	r.HandleFunc("/api/v1/object/{name}", nil)
+	r.HandleFunc("/api/v1/objects", nil)
 	//domains
-	r.HandleFunc("/api/1/domain/{name}", ds.apiDomainHandler).Methods("POST", "DELETE", "PUT", "GET")
-	r.HandleFunc("/api/1/domains", ds.apiDomainsHandler).Methods("GET")
+	r.HandleFunc("/api/v1/domain/{name}", ds.apiDomainHandler).Methods("POST", "DELETE", "PUT", "GET")
+	r.HandleFunc("/api/v1/domains", ds.apiDomainsHandler).Methods("GET")
 	//vars
-	r.HandleFunc("/api/1/var/{name}/value/{value}", ds.apiVarValueHandler).Methods("POST", "DELETE", "PUT", "GET")
-	r.HandleFunc("/api/1/var/{name}", ds.apiVarHandler).Methods("POST", "DELETE", "PUT", "GET")
-	r.HandleFunc("/api/1/vars", ds.apiVarsHandler).Methods("GET")
+	r.HandleFunc("/api/v1/var/{name}/value/{value}", ds.apiVarValueHandler).Methods("POST", "DELETE", "PUT", "GET")
+	r.HandleFunc("/api/v1/var/{name}", ds.apiVarHandler).Methods("POST", "DELETE", "PUT", "GET")
+	r.HandleFunc("/api/v1/vars", ds.apiVarsHandler).Methods("GET")
 	//groups
-	r.HandleFunc("/api/1/group/{name}/var/{varName}/value/{value}", ds.apiGroupHandler).Methods("POST", "DELETE", "PUT", "GET")
-	r.HandleFunc("/api/1/group/{name}/vars", ds.apiGroupHandler).Methods("GET")
-	r.HandleFunc("/api/1/group/{name}/host/{hostName}", ds.apiGroupHandler).Methods("POST", "DELETE", "PUT", "GET")
-	r.HandleFunc("/api/1/group/{name}/hosts", ds.apiGroupHandler).Methods("GET")
-	r.HandleFunc("/api/1/group/{name}", ds.apiGroupHandler).Methods("POST", "DELETE", "PUT", "GET")
-	r.HandleFunc("/api/1/groups", ds.apiGroupsHandler).Methods("GET")
+	r.HandleFunc("/api/v1/group/{name}/var/{varName}/value/{value}", ds.apiGroupHandler).Methods("POST", "DELETE", "PUT", "GET")
+	r.HandleFunc("/api/v1/group/{name}/vars", ds.apiGroupHandler).Methods("GET")
+	r.HandleFunc("/api/v1/group/{name}/host/{hostName}", ds.apiGroupHandler).Methods("POST", "DELETE", "PUT", "GET")
+	r.HandleFunc("/api/v1/group/{name}/hosts", ds.apiGroupHandler).Methods("GET")
+	r.HandleFunc("/api/v1/group/{name}", ds.apiGroupHandler).Methods("POST", "DELETE", "PUT", "GET")
+	r.HandleFunc("/api/v1/groups", ds.apiGroupsHandler).Methods("GET")
 	//hosts
-	r.HandleFunc("/api/1/host/{name}/var/{varName}/value/{value}", ds.apiHostVarHandler).Methods("POST", "DELETE", "PUT", "GET")
-	r.HandleFunc("/api/1/host/{name}/vars", ds.apiHostVarsHandler).Methods("GET")
-	r.HandleFunc("/api/1/host/{name}", ds.apiHostHandler).Methods("POST", "DELETE", "PUT", "GET")
-	r.HandleFunc("/api/1/hosts", ds.apiHostsHandler).Methods("GET")
+	r.HandleFunc("/api/v1/host/{name}/var/{varName}/value/{value}", ds.apiHostVarHandler).Methods("POST", "DELETE", "PUT", "GET")
+	r.HandleFunc("/api/v1/host/{name}/vars", ds.apiHostVarsHandler).Methods("GET")
+	r.HandleFunc("/api/v1/host/{name}", ds.apiHostHandler).Methods("POST", "DELETE", "PUT", "GET")
+	r.HandleFunc("/api/v1/hosts", ds.apiHostsHandler).Methods("GET")
 	//ansible
 	r.HandleFunc("/api/ansible/domain/{name}", ds.ansibleHandler).Methods("GET")
-
+	//handle root requests
 	http.Handle("/", r)
 
 	log.Infoln("Staring webserver")
